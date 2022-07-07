de2d709f9ee4f08d9b2951936b2019508942e216
Andrey Moskalenko <43238782+AM0SK@users.noreply.github.com>
2022-07-07 11:13:54 +0300

    Update global-footer-code.js

--- global-footer-code.js
+++ global-footer-code.js
@@ -1,2 +1,2 @@
-const src = "//web.webformscr.com/apps/fc3/build/loader.js",
+const src = "//web.webformscr.com/apps/fc3/build/loader.js";
 const id  = "06606c63d71d70834fe8acb4c0c59c6b2ee3cc3dc03d05abcdbd3a2a2c66a19a";
