9af0f46043f0e47b449561c4ffdb209d64498f44
Andrey Moskalenko <43238782+AM0SK@users.noreply.github.com>
2022-07-07 16:02:33 +0300

    Update global-form.js

--- global-form.js
+++ global-form.js
@@ -1,3 +1,4 @@
+
 // Animate lottie after submit
 let lottie, animations;
 // Return a promise that resolves to true once animation is loaded
@@ -38,21 +39,31 @@ Webflow.push(() => {
 // After submit
 function isSpForm() {
     if ($('form[class^=sp-]').length) {
-        const spForm = $('form[class^=sp-]');
-        const spInputs = $(spForm).find('input');
-        const spButton = $(spForm).find('button');
+        const spForm = $('form[class^=sp-]'),
+            spInputs = $(spForm).find('input'),
+            spButton = $(spForm).find('button');
 
         $('.w-input[name=email]').on('keyup', function() {
             $(spInputs[0]).val($(this).val());
         });
         $('form[id^=wf-form-]').submit(function(e) {
-            let lottieAttr = $(e.target).find('[data-lottie]').attr('data-lottie');
-            let textAnim = $(e.target).find(':submit').siblings('.overflow-hidden');
-            $(e.target).find('input:not(:submit)').val('');
-            $(textAnim).fadeTo(300, 0)
+            let formSubmitted = $(e.target),
+                lottieAttr = $(formSubmitted).find('[data-lottie]').attr('data-lottie'),
+                isActive = $(formSubmitted).find('[data-form-submitted=active]').attr('data-form-submitted');
+            $(formSubmitted).find('input:not(:submit)').val('');
+
+
+            if (typeof isActive !== 'undefined' && isActive !== false) {
+                $(formSubmitted).find('[data-form-submitted=active]').addClass('active');
+            }
+
+            $(formSubmitted).find('[data-form-submitted=hide]').fadeTo(300, 0);
             lottie.play(lottieAttr);
             setTimeout(() => {
-                $(textAnim).fadeTo(300, 1)
+                if (typeof isActive !== 'undefined' && isActive !== false) {
+                    $(formSubmitted).find('[data-form-submitted=active]').removeClass('active');
+                }
+                $(formSubmitted).find('[data-form-submitted=hide]').fadeTo(300, 1)
                 lottie.stop(lottieAttr)
             }, 3000);
 
